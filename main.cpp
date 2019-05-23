f9a9d2b69f2256f348fa807c8f614e586f398d2d
Jay Montoya <jayjoplin9898@gmail.com>
2019-05-23 14:43:05 -0700

    documentation update

--- main.cpp
+++ main.cpp
@@ -9,6 +9,13 @@
  *     The function should throw an exception if it is passed an integer that is not a perfect square. Demonstrate the
  *     function with a suitable driver program
  *
+ * ALGORITHM:
+ *      1. Prompt user for a number that is a perfect square.
+ *      2. Try to get the root by calling the function.
+ *      3. Catch the exception when thrown.
+ *      4. Promp the user for a non-perfect square.
+ *      5. Try to get the root by calling the function.
+ *      6. Catch the exception when thrown.
  *
  *
  */
@@ -16,6 +23,7 @@
 #include <cmath>
 using namespace std;
 
+// function prototypes
 int getRoot(int num);
 
 class NotAPerfectSquareException {
@@ -35,21 +43,29 @@ void NotAPerfectSquareException::printMessage() {
 
 
 int main() {
+    // greeting
     cout << "Welcome to the demo of project 2!" << endl;
 
+    // number to enter
     int num;
 
     cout << "Enter a perfect square: ";
     cin >> num;
+    // try to get the Root
     try {
         cout << "\tROOT IS:" << getRoot(num) << endl;
+    // catch the exception for a not so perfect square
     } catch(NotAPerfectSquareException e) {
         e.printMessage();
     }
+
+    // prompt user for a not so perfect square
     cout << "Enter a NOT SO perfect square: ";
     cin >> num;
+    // try to get the Root
     try {
         cout << "\tROOT IS:" << getRoot(num) << endl;
+    // catch the exception for a not so perfect square
     } catch(NotAPerfectSquareException e) {
         e.printMessage();
     }
